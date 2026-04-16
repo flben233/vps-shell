a5380f5e23444e982477f87cd6e1f9addf60431e
ShirakawaTyu <40626158+flben233@users.noreply.github.com>
2026-04-16 16:16:27 +0800

    Update autoBestTrace.sh

--- autoBestTrace.sh
+++ autoBestTrace.sh
@@ -19,11 +19,10 @@ next
 ip_list=(219.141.147.210 202.96.209.133 58.60.188.222 202.106.50.1 210.22.97.1 210.21.196.6 221.179.155.161 211.136.112.200 120.196.165.24 202.112.14.151)
 ip_addr=(北京电信 上海电信 深圳电信 北京联通 上海联通 深圳联通 北京移动 上海移动 深圳移动 成都教育网)
 # ip_len=${#ip_list[@]}
-
+echo '关于软件卸载，因为nexttrace是绿色版单文件，卸载只需输入命令 rm /usr/local/bin/nexttrace 即可'
 for i in {0..9}
 do
 	echo ${ip_addr[$i]}
-	echo '关于软件卸载，因为nexttrace是绿色版单文件，卸载只需输入命令 rm /usr/local/bin/nexttrace 即可'
 	nexttrace -M ${ip_list[$i]}
 	next
 done
